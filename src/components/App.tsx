078519f0ae374f6e09e35d717d6c0485ace2713a
Genimax <urdnot.wrexx21@gmail.com>
2023-02-03 01:37:38 +0700

    media fixed

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -15,7 +15,7 @@ class App extends React.Component {
     return (
       <Routes>
         <Route
-          path="*"
+          path="/*"
           element={
             <>
               <Header />
